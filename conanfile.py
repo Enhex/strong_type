5266e9335521f1eb4c5e04c6f88abda0b16f8821
Enhex <enhex0@gmail.com>
2022-02-28 05:16:30 +0200

    improve recipe

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools
 
 
 class StrongtypeConan(ConanFile):
@@ -6,8 +6,11 @@ class StrongtypeConan(ConanFile):
 	version = "master"
 	license = "MIT"
 	description = "Create new type from existing type without changing the interface."
-	# No settings/options are necessary, this is header only
+	homepage = "https://github.com/Enhex/strong_type"
 	exports_sources = "include/*"
 
 	def package(self):
 		self.copy("*.h")
+
+	def package_id(self):
+		self.info.header_only()
